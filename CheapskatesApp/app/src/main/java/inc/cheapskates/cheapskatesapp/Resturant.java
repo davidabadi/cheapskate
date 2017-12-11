d0ea4ec66dded93409f07834aebb9b71557decc8
davidabadi <david@abadi.org>
2017-12-11 10:22:20 -0500

    Added comments and deleted extra code

--- CheapskatesApp/app/src/main/java/inc/cheapskates/cheapskatesapp/Resturant.java
+++ CheapskatesApp/app/src/main/java/inc/cheapskates/cheapskatesapp/Resturant.java
@@ -2,36 +2,18 @@ package inc.cheapskates.cheapskatesapp;
 
 import java.io.Serializable;
 
-
+//Creates restaurant class
 public class Resturant implements Serializable {
-    String restaurant;
-    String apikey;
-    String id;
-    String name;
-    String longitude;
-    String latitude;
-    String address;
-    String rating;
-    String locality;
+    private String id;
+    private String name;
+    private String longitude;
+    private String latitude;
+    private String address;
+    private String rating;
+    private String locality;
 
-    String url;
 
-    public String getRestaurant() {
-        return restaurant;
-    }
-
-    public void setRestaurant(String restaurant) {
-        this.restaurant = restaurant;
-    }
-
-    public String getApikey() {
-        return apikey;
-    }
-
-    public void setApikey(String apikey) {
-        this.apikey = apikey;
-    }
-
+    //getters and setters for the members of the class
     public String getId() {
         return id;
     }
@@ -46,14 +28,6 @@ public class Resturant implements Serializable {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 
     public String getLongitude(){return longitude;}
--- CheapskatesApp/app/src/main/java/inc/cheapskates/cheapskatesapp/food_categories.java
+++ CheapskatesApp/app/src/main/java/inc/cheapskates/cheapskatesapp/food_categories.java
@@ -71,12 +71,11 @@ public class food_categories extends AppCompatActivity {
 
     //setting up mybudget variable
     private EditText mybudget;
-//created a string to store the category chosen
-
+    //created a string to store the category chosen
     String category;
-    //sets up the go button to read from the text box and change budget
 
 
+    //sets up and pass the cuisine id according to zomato
     public void americansbutton(View v)
     {
         Button button = (Button) v;
@@ -87,6 +86,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void bakerybutton(View v)
     {
         Button button = (Button) v;
@@ -97,6 +97,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void beveragesbutton(View v)
     {
         Button button = (Button) v;
@@ -107,6 +108,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void breakfastbutton(View v)
     {
         Button button = (Button) v;
@@ -117,6 +119,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void burgerbutton(View v)
     {
         Button button = (Button) v;
@@ -127,6 +130,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void chinesebutton(View v)
     {
         Button button = (Button) v;
@@ -137,6 +141,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void dessertbutton(View v)
     {
         Button button = (Button) v;
@@ -147,6 +152,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void fastfoodbutton(View v)
     {
         Button button = (Button) v;
@@ -157,6 +163,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void frenchbutton(View v)
     {
         Button button = (Button) v;
@@ -167,6 +174,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void icecreambutton(View v)
     {
         Button button = (Button) v;
@@ -177,6 +185,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void indianbutton(View v)
     {
         Button button = (Button) v;
@@ -187,6 +196,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void italianbutton(View v)
     {
         Button button = (Button) v;
@@ -197,6 +207,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void japanesebutton(View v)
     {
         Button button = (Button) v;
@@ -207,6 +218,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void koreanbutton(View v)
     {
         Button button = (Button) v;
@@ -217,6 +229,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void mediterraneanbutton(View v)
     {
         Button button = (Button) v;
@@ -227,6 +240,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void mexicanbutton(View v)
     {
         Button button = (Button) v;
@@ -236,6 +250,8 @@ public class food_categories extends AppCompatActivity {
         setCuisine(this, Integer.toString(cuisine));
         startActivity(intent);
     }
+
+    //sets up and pass the cuisine id according to zomato
     public void pizzabutton(View v)
     {
         Button button = (Button) v;
@@ -246,6 +262,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void seafoodbutton(View v)
     {
         Button button = (Button) v;
@@ -255,6 +272,8 @@ public class food_categories extends AppCompatActivity {
         setCuisine(this, Integer.toString(cuisine));
         startActivity(intent);
     }
+
+    //sets up and pass the cuisine id according to zomato
     public void southernbutton(View v)
     {
         Button button = (Button) v;
@@ -265,6 +284,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //sets up and pass the cuisine id according to zomato
     public void thaibutton(View v)
     {
         Button button = (Button) v;
@@ -274,6 +294,8 @@ public class food_categories extends AppCompatActivity {
         setCuisine(this, Integer.toString(cuisine));
         startActivity(intent);
     }
+
+    //sets up and pass the cuisine id according to zomato
     public void vegetarianbutton(View v)
     {
         Button button = (Button) v;
@@ -284,6 +306,7 @@ public class food_categories extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //the cuisine id is set up in shared preferences
     public static void setCuisine(Context context, String cuisine)
     {
         SharedPreferences prefs5 = context.getSharedPreferences("Cuisine", 0);
@@ -292,76 +315,11 @@ public class food_categories extends AppCompatActivity {
         editor5.apply();
     }
 
+    //Go back to the home page
     public void homebutton(View v) {
         Button button = (Button) v;
         Intent intent = new Intent(this, HomeActivity.class);
         v.findViewById(R.id.home_button);
         startActivity(intent);
     }
-
-
-    /*    enum OffersOrder {("cuisines":"seafood");
-        String sound;
-        OffersOrder(String s) { sound = s; }
-    }
-
-    List<OffersOrder> l = new ArrayList<>();
-
-// code to populate your list
-...
-
-// sorting
-
-        java.util.Collections.sort(l, new Comparator<OffersOrder>(){
-        @Override
-        public int compare(OffersOrder o1, category) {
-
-
-            if(Objects.equals(category, o1.get("cuisines"))) //compares the category selected to the
-        {
-            //List<Objects>
-        }
-
-            // this method should return a negative value if o1 < o2
-            // this method should return 0 if o1 == o2
-            // this method should return a positive value if o1 > o2
-            return 0;
-        }
-    });
-
-
-    if Jsonstring
-
-       /*BagelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentLoadListOfRestaurants = new Intent(food_categories.this, ListOfRestaurantsActivity.class);
-                startActivity(intentLoadListOfRestaurants);
-            }
-        });*/
-
-
-//This sorts the JSON string by name
-    /*String myJSONstring;
-    String restaurantName;
-    JsonElement restaurantJSONElement;
-    JsonPrimitive restaurantJSONPrimitive;
-
-    JsonParser parser = new JsonParser();
-    // myJSONstring is the raw JSON data
-    JsonElement parentJSONElement = parser.parse(myJSONstring);
-    JsonObject parentJSONObject = parentJSONElement.getAsJsonObject();
-    JsonArray restaurantsJSONArray = (JsonArray) parentJSONObject.get("restaurants");
-//for(int i=0;i<restaurantsJSONArray.size(); i++)  //for some reason size was undeclared dont know why
-    int i = 0;
-    for(i = 0; i < 20; i++)
-    {
-        //get the element
-        restaurantJSONElement = ((JsonObject)restaurantsJSONArray.get(i)).get("restaurant");
-        //get the restaurant name
-        restaurantJSONPrimitive = ((JsonObject)restaurantJSONElement.getAsJsonObject()).getAsJsonPrimitive("name");
-        restaurantName = restaurantJSONPrimitive.getAsString();
-    }*/
-
-
 }
